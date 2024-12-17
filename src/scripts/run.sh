1454c08acd56d702f959c314846d88e80fc3b8e6
3rdCore <tom.marty@mila.quebec>
2024-12-17 17:11:36 -0500

    first hyp-search

--- src/scripts/run.sh
+++ src/scripts/run.sh
@@ -12,4 +12,7 @@ python train.py --multirun hydra/launcher=mila_tom save_dir=/home/mila/t/tom.mar
 
 # For launching a single job, you can use the same command without --multirun. Make sure to provide a single value for each argument :
 
-python train.py hydra/launcher=mila_tom save_dir=/home/mila/t/tom.marty/scratch/unregistered_logs seed=0 my_custom_argument="config_1" 
+python train.py hydra/launcher=mila_tom save_dir=/home/mila/t/tom.marty/scratch/unregistered_logs seed=0 my_custom_argument="config_1"
+
+
+python train.py  --multirun hydra/launcher=mila_tom save_dir=/home/mila/t/tom.marty/scratch/unregistered_logs seed=0,1,2 ++task.temperature=0.1,0.5,1,5 ++task.lr=1e-4,1e-3 ++task.is_beta_VAE=False ++SCM.interventional_shift=10 ++train_dataset.observational_density=0.1,0.5,0.8 ++train_dataset.shift=0.0,1.0 logger.tags=[hyp-search]
